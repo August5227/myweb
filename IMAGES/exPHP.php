a8d395f327a93af34efed56b7e9aafaef1711bd3
August5227 <nobpakon.ten@pccpl.ac.th>
2022-02-02 16:45:23 +0700

    first commit

---
+++ IMAGES/exPHP.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+ <html>
+     <head>
+         <title>
+             PHP Syntax...
+</title> 
+</head>
+      <body>
+        <h1>Content from HTML </h1>
+        <?php
+            echo "Content from PHP";
+        ?>
+    </body>
+</html>
